8f355b3d378b31147814df8473d1a2957cd8030e
hmkiff <kiffel@wisc.edu>
2024-02-27 15:13:38 -0600

    removed build directory from git

--- build/libraries/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/hmkif/OneDrive/Desktop/Open Projects/base_project/libraries/include
-# Build directory: C:/Users/hmkif/OneDrive/Desktop/Open Projects/base_project/build/libraries/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/hmkif/OneDrive/Desktop/Open Projects/base_project/src
-# Build directory: C:/Users/hmkif/OneDrive/Desktop/Open Projects/base_project/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
